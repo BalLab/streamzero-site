663a921c2c9b98efcc7f971453ba3738f0a7602a
bal balaji <bal.balaji@juliusbaer.com>
2023-08-25 21:04:13 +0200

    update event ticker, add screenshots

--- content/en/products/k8x/rename.py
+++ content/en/products/k8x/rename.py
@@ -7,4 +7,4 @@ file_list = os.listdir(dir_path)
 for file_name in file_list:
     #print(file_name)
     if '.svg' in file_name:
-       print(f'<div class="slide"><img class="alignnone" src="/images/connectors/{file_name}" alt="" width="60" height="60" /></div>')
+       print(f'<div class="slide"><button class="image-button"><img src="/images/connectors/{file_name}" alt="Image"><span>{file_name.split(".")[0]}</span></button></div>')
